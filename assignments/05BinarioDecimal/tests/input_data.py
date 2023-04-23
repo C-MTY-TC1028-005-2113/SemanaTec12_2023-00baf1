ca88e999f25da63f17503577a2857bcd3dc37b04
roque63 <roque@itesm.mx>
2023-04-22 18:59:42 -0600

    Initial commit

---
+++ assignments/05BinarioDecimal/tests/input_data.py
@@ -0,0 +1,63 @@
+# List of tuples
+# Each tuple contains a test:
+# - the first element are the inputs,
+# - the second are the output,
+# - and the third is the message in case of an error
+# To test another case, add another tuple
+
+input_values = [
+    # Test case 1
+    (
+            [1],
+        ["",
+"1 I Am Not an Animal",
+"2 Chernobyl",
+"3 Rick and Morty",
+"4 Breaking Bad",
+"5 Hunter x Hunter",
+"6 Sherlock",
+"7 Planet Earth II",
+"8 Peaky Blinders",
+"9 Stranger Things",
+"10 DEATH NOTE",
+"11 Avatar: The Last Airbender",
+"12 The Twilight Zone",
+"13 The Wire",
+"14 Gravity Falls",
+"15 The Sopranos",
+"16 Neon Genesis Evangelion",
+"17 The Marvelous Mrs. Maisel",
+"18 Young Justice",
+"19 Band of Brothers",
+"20 Futurama",
+"Animation 9",
+"Drama 6",
+"Crime 3",
+"Documentary 1",
+"Comedy 1",
+"Total: 20",
+"Calificacion Promedio: 8.38",
+"Tiempo Promedio: 893.2"],
+"Debe salir: lo que te indica el caso de prueba1"
+    ),
+ # Test case 2
+    (
+ [2],
+ ["",
+"1 Josu",
+"2 Cristian",
+"3 Ricky",
+"4 Beatriz",
+"5 Hugo",
+"6 Silvia",
+"7 Pedro",
+"ITC 3",
+"INE 1",
+"IFI 2",
+"IRS 1",
+"Total: 7",
+"Calificacion Promedio: 88.71",
+"Tiempo Promedio: 2514.29"],
+"Debe salir:"
+    )
+]
